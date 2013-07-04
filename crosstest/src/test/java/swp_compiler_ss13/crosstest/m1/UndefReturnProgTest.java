c00c218292262765b7fffc91f4c54b3d2c0b8f3e
jvf@home <web@jens-fischer.eu>
2013-07-04 21:28:32 +0200

    Add m1 tests for new test setup

---
+++ crosstest/src/test/java/swp_compiler_ss13/crosstest/m1/UndefReturnProgTest.java
@@ -0,0 +1,28 @@
+package swp_compiler_ss13.crosstest.m1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import swp_compiler_ss13.crosstest.AbstractCrosstest;
+import swp_compiler_ss13.crosstest.Compiler;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class UndefReturnProgTest extends AbstractCrosstest {
+
+	public UndefReturnProgTest(String testname, Class lexerToUse, Class parserToUse, Class analyserToUse, Class irgenToUse,
+							   Class backToUse) {
+		this.compiler = new Compiler(lexerToUse, parserToUse, analyserToUse, irgenToUse, backToUse);
+		assumeAllModulesPreset();
+	}
+
+	@Parameterized.Parameters(name= "{index}: {0}")
+	public static Collection<Object[]> data() {
+		return moduleCombinations();
+	}
+
+	@Override
+	protected String getProgName() {
+		return "undefReturnProg";
+	}
+}

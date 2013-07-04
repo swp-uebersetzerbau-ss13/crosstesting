be260986ab90cbb81f9d1fffdae9f289f9a673e1
jvf@home <web@jens-fischer.eu>
2013-07-04 21:43:07 +0200

    Add additional tests for new test setup

---
+++ crosstest/src/test/java/swp_compiler_ss13/crosstest/additional/ReturnProgTest.java
@@ -0,0 +1,28 @@
+package swp_compiler_ss13.crosstest.additional;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import swp_compiler_ss13.crosstest.AbstractCrosstest;
+import swp_compiler_ss13.crosstest.Compiler;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ReturnProgTest extends AbstractCrosstest {
+
+	public ReturnProgTest(String testname, Class lexerToUse, Class parserToUse, Class analyserToUse, Class irgenToUse,
+						  Class backToUse) {
+		this.compiler = new Compiler(lexerToUse, parserToUse, analyserToUse, irgenToUse, backToUse);
+		assumeAllModulesPreset();
+	}
+
+	@Parameterized.Parameters(name= "{index}: {0}")
+	public static Collection<Object[]> data() {
+		return moduleCombinations();
+	}
+
+	@Override
+	protected String getProgName() {
+		return "returnProg()";
+	}
+}

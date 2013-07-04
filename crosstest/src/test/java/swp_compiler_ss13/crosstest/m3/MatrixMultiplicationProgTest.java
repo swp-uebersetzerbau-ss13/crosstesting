97042f6d50fe375f52923ba7366c8abdf94a6330
jvf@home <web@jens-fischer.eu>
2013-07-05 01:05:59 +0200

    Add m3 tests

---
+++ crosstest/src/test/java/swp_compiler_ss13/crosstest/m3/MatrixMultiplicationProgTest.java
@@ -0,0 +1,30 @@
+package swp_compiler_ss13.crosstest.m3;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import swp_compiler_ss13.crosstest.AbstractCrosstest;
+import swp_compiler_ss13.crosstest.Compiler;
+
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+@Ignore("doesn't terminate in LexerJb->ParserImpl->SemanticAnalyserJb->IntermediateCodeGeneratorImpl->BackendJb")
+public class MatrixMultiplicationProgTest extends AbstractCrosstest {
+
+	public MatrixMultiplicationProgTest(String testname, Class lexerToUse, Class parserToUse, Class analyserToUse, Class irgenToUse,
+										Class backToUse) {
+		this.compiler = new Compiler(lexerToUse, parserToUse, analyserToUse, irgenToUse, backToUse);
+		assumeAllModulesPreset();
+	}
+
+	@Parameterized.Parameters(name= "{index}: {0}")
+	public static Collection<Object[]> data() {
+		return moduleCombinations();
+	}
+
+	@Override
+	protected String getProgName() {
+		return "matrixMultiplicationProg";
+	}
+}

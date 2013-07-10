48ab392d1a71fb63930830daa2ae15a6f2084f10
jvf@home <web@jens-fischer.eu>
2013-07-10 19:43:41 +0200

    Allow different floating point precision in NewtonProg

--- crosstest/src/test/java/swp_compiler_ss13/crosstest/m3/NewtonProgTest.java
+++ crosstest/src/test/java/swp_compiler_ss13/crosstest/m3/NewtonProgTest.java
@@ -1,19 +1,23 @@
 package swp_compiler_ss13.crosstest.m3;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import swp_compiler_ss13.crosstest.AbstractCrosstest;
 import swp_compiler_ss13.crosstest.Compiler;
+import swp_compiler_ss13.fuc.backend.LLVMBackend;
+import swp_compiler_ss13.javabite.backend.BackendJb;
 
 import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class NewtonProgTest extends AbstractCrosstest {
 
+	private Class backendToUse;
+
 	public NewtonProgTest(String testname, Class lexerToUse, Class parserToUse, Class analyserToUse, Class irgenToUse,
-						  Class backToUse) {
-		this.compiler = new Compiler(lexerToUse, parserToUse, analyserToUse, irgenToUse, backToUse);
+						  Class backendToUse) {
+		this.compiler = new Compiler(lexerToUse, parserToUse, analyserToUse, irgenToUse, backendToUse);
+		this.backendToUse = backendToUse;
 		assumeAllModulesPreset();
 	}
 
@@ -25,5 +29,17 @@ public class NewtonProgTest extends AbstractCrosstest {
 	@Override
 	protected String getProgName() {
 		return "newtonProg";
+	}
+
+	@Override
+	protected String getExpectedOutput() {
+		/* allow different floating point precision */
+		if (backendToUse == BackendJb.class) {
+			return "i hate floating point numbers1.4142156862745097\n";
+		} else if (backendToUse == LLVMBackend.class) {
+			return "i hate floating point numbers1.414216e+00\n";
+		} else {
+			return super.getExpectedOutput();
+		}
 	}
 }
